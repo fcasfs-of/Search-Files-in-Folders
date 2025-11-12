cfa1f77a2ec1ac6c6931ee21fdd53d507eda8a4f
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-11-12 10:20:42 -0300

    Update run.js

--- run.js
+++ run.js
@@ -10,7 +10,9 @@ var dversion="1.0.0.0";
       resourcesTitle: "Recursos",
       aboutTitle: "Sobre o Aplicativo",
       aboutText: "O aplicativo permite selecionar uma pasta, escolher o formato do arquivo, inserir um texto e buscar os arquivos correspondentes na pasta.",
-      resourceslistText:"<li>Escolha uma pasta</li><li>Selecione o formato do arquivo</li><li>Insira o texto desejado</li><li>Clique no botão Buscar</li>"
+      resourceslistText:"<li>Escolha uma pasta</li><li>Selecione o formato do arquivo</li><li>Insira o texto desejado</li><li>Clique no botão Buscar</li>",
+      NewslistText:"",
+      NewslistTitle:"Novas Funcionalidades da Nova Versão"
     },
     es: {
       appTitle: "Buscar Archivo en Carpetas",
@@ -20,7 +22,9 @@ var dversion="1.0.0.0";
       resourcesTitle: "Recursos",
       aboutTitle: "Acerca de la Aplicación",
       aboutText: "La aplicación permite seleccionar una carpeta, elegir el formato del archivo, ingresar un texto y buscar los archivos correspondientes en la carpeta.",
-      resourceslistText:"<li>Elige una carpeta</li><li>Selecciona el formato de archivo</li><li>Introduce el texto deseado</li><li>Haz clic en el botón Examinar</li>"
+      resourceslistText:"<li>Elige una carpeta</li><li>Selecciona el formato de archivo</li><li>Introduce el texto deseado</li><li>Haz clic en el botón Examinar</li>",
+      NewslistText:"",
+      NewslistTitle:"Nuevas Características de la Nueva Versión"
     },
     en: {
       appTitle: "Search Files in Folders",
@@ -30,7 +34,9 @@ var dversion="1.0.0.0";
       resourcesTitle: "Features",
       aboutTitle: "About the App",
       aboutText: "The app allows selecting a folder, choosing the file format, entering text, and searching files in the folder.",
-      resourceslistText:"<li>Choose a folder</li><li>Select the file format</li><li>Enter the desired text</li><li>Click the Browse button</li>"
+      resourceslistText:"<li>Choose a folder</li><li>Select the file format</li><li>Enter the desired text</li><li>Click the Browse button</li>",
+      NewslistText:"",
+      NewslistTitle:"New Features of the New Version"
     }
   };
 
@@ -43,6 +49,9 @@ var dversion="1.0.0.0";
   const aboutTitle = document.getElementById('about-title');
   const aboutText = document.getElementById('about-text');
   const resourceslistText = document.getElementById('resources-list');
+
+  const newslistText = document.getElementById('news-list');
+  const newslistTtitle = document.getElementById('news-title');
 
   const langPtBtn = document.getElementById('lang-pt');
   const langEsBtn = document.getElementById('lang-es');
@@ -59,6 +68,8 @@ var dversion="1.0.0.0";
     aboutTitle.textContent = t.aboutTitle;
     aboutText.textContent = t.aboutText;
     resourceslistText.innerHTML = t.resourceslistText;
+    newslistText.innerHTML = t.NewslistText;
+    newslistTtitle.textContent = t.NewslistTitle;
   }
 
   // Event listeners
