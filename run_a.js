290ce86c11a7b794ef3cc06bf0247823190b17a9
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-03-12 09:27:39 -0300

    Update run_a.js

--- run_a.js
+++ run_a.js
@@ -3,6 +3,7 @@
   const body = document.body;
 
   const appTcss = document.getElementById('csscustom');
+  const appnames = document.getElementById('nameapp');
 
   const titleEl = document.getElementById('title');
   const messageEl = document.getElementById('message');
@@ -10,18 +11,20 @@
   const iconContainer = document.getElementById('error-icon');
   
   function setLanguage(lang) {
+    if (iconContainer && homeLinkEl && messageEl && titleEl && appnames && appTcss){
     currentLang = lang;
     const msg = messages[lang];
-    if (msg && messages){  
+    if (msg && messages && translations){  
     localStorage.setItem("lang", currentLang);
 
+    appnames.innerHTML = ""+translations[currentLang].appTitle+"";
     appTcss.innerHTML = "<style> [data-lang=\""+lang+"\"] {  background: #e0e0e0 !important;   opacity:0.7;  pointer-events:none;  }  </style>";
 
     titleEl.innerHTML = msg.title;
     messageEl.innerHTML = msg.message;
     homeLinkEl.innerHTML = msg.home;
-    iconContainer.innerHTML = msg.icon;
-      }  else {  setLanguage("pt");   }
+    iconContainer.innerHTML = "<br/><br/>"+msg.icon;
+      }  else {  setLanguage("pt");   }   }
   }
 
   document.querySelectorAll('.language-selector button').forEach(btn => {
@@ -34,7 +37,7 @@
   if(localStorage.getItem("lang")!=""){  setLanguage(localStorage.getItem("lang"));  }  else {  setLanguage(currentLang);   }
 
 
-if(localStorage.getItem('tema') === 'escuro') {   body.classList.add('dark');       } else {   body.classList.remove('dark');      }
+if(body){   if(localStorage.getItem('tema') === 'escuro') {   body.classList.add('dark');       } else {   body.classList.remove('dark');      }    }
 
 
 
