ada07e10dbeebf81e667e1c3711096ddb6371cbf
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-03-12 09:03:21 -0300

    Update run_a_lang.js

--- run_a_lang.js
+++ run_a_lang.js
@@ -1,4 +1,4 @@
-var error_icond= `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="24" height="24">      <circle cx="32" cy="32" r="30" stroke="#ff6347" stroke-width="4" fill="#fff"/>    <line x1="20" y1="20" x2="44" y2="44" stroke="#ff6347" stroke-width="4"/>     <line x1="44" y1="20" x2="20" y2="44" stroke="#ff6347" stroke-width="4"/> </svg>`;
+var error_icond= `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="64" height="64">      <circle cx="32" cy="32" r="30" stroke="#ff6347" stroke-width="4" fill="#fff"/>    <line x1="20" y1="20" x2="44" y2="44" stroke="#ff6347" stroke-width="4"/>     <line x1="44" y1="20" x2="20" y2="44" stroke="#ff6347" stroke-width="4"/> </svg>`;
 
 
 const messages = {
